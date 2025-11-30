8e87723b57c5dfeadfd65f2fff64b44c5789ebae
Fordb123 <160549425+Fordb123@users.noreply.github.com>
2025-11-30 01:35:22 -0300

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,5 +1,5 @@
 (function() {
-    fetch('https://cdn.jsdelivr.net/gh/yeorinhieut/novel-dl/script.min.js')
+    fetch('https://raw.githubusercontent.com/Fordb123/novel-dl.All/main/script.js?t=' + Date.now())
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Failed to fetch script: ${response.statusText}`);
